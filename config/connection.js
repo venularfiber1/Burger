69174273c03da9a0a96a7959988c7cd673e03e2b
unknown <saransh1@gmail.com>
2018-08-28 01:18:33 -0400

    Jaw

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,20 @@
 
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+if(process.env.JAWDB_URL){
+  connection = mysql.createConnection(process.env.JAWDB_URL);
+
+}
+else{connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "root",
   database: "burgers_db"
 });
-
+};
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
